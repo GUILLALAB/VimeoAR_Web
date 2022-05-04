a018c15232b6976d19c9b80867f4b41247666bc0
Guillaouic Franck <FOXspin@hotmail.fr>
2022-05-04 10:07:13 +0200

    ready to deploy

--- server.js
+++ server.js
@@ -97,6 +97,25 @@ app.get('/vimeo/api', (request, response) => {
   );
 });
 
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!"
+  });
+});
+
+router.get('/test',(req,res) => {
+    res.json({
+        hello: "test!"
+      });
+
+})
+
+router.post('/testpost',(req,res) => {
+    res.json({
+        hello: "hit the POST!"
+      });
+})
+
 app.use(`/.netlify/functions/api`, router);
 
 const listener = app.listen(process.env.PORT, () => {
