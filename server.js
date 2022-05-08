63cb870a93c5c074f8d08535f369ba6eaf151f4d
Guillaouic Franck <FOXspin@hotmail.fr>
2022-05-08 18:00:51 +0200

    first commit

--- server.js
+++ server.js
@@ -232,7 +232,7 @@ app.get('/rtm/:uid/', nocache , generateRTMToken);
 app.get('/rte/:channel/:role/:tokentype/:uid', nocache , generateRTEToken);
 
 function getToken(){
-  app.get('/rtc/:channel/:role/:tokentype/:uid', nocache , generateRTCToken);
+  app.get('https://livear.herokuapp.com/rtc/web/publisher/uid/1', nocache , generateRTCToken);
 }
 const listener = app.listen(process.env.PORT, () => {
   console.log(`[Server] Running on port: ${listener.address().port} 🚢`);
