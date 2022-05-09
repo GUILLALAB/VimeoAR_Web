ded71a3336e082f9947585050b5335cece752b31
Guillaouic Franck <FOXspin@hotmail.fr>
2022-05-09 10:20:23 +0200

    first commit

--- assets/js/webvr-broadcast-client.js
+++ assets/js/webvr-broadcast-client.js
@@ -218,46 +218,37 @@ function createBroadcaster(streamId) {
   video.setAttribute('webkit-playsinline', 'webkit-playsinline');
   video.setAttribute('playsinline', 'playsinline');
   video.setAttribute('poster', '/imgs/no-video.jpg');
-  console.log(video);
-  // add video object to the DOM
-  document.querySelector('a-assets').appendChild(video);
 
-  // configure the new broadcaster
-  const gltfModel = '#broadcaster';
-  const scale = '1 1 1'; 
-  const offset = streamCount;
-  const position = offset + ' -1 0';
-  const rotation = '0 0 0';
+    // configure the new broadcaster
+    const gltfModel = '#broadcaster';
+    const scale = '1 1 1'; 
+    const offset = streamCount;
+    const position = offset + ' -1 0';
+    const rotation = '0 0 0';
+  
+    video.setAttribute('scale', scale);
+    video.setAttribute('position', position);
+    video.setAttribute('rotation', rotation);
+
+    video.setAttribute('geometry', {
+      primitive: 'plane',
+      height: 3,
+      width: 1
+    });
+
+    var texture = new THREE.VideoTexture(video);
+    texture.minFilter = THREE.LinearFilter; 
+    texture.magFilter = THREE.LinearFilter; 
+    texture.flipY = false;
+    video.setAttribute('material', 'src', texture)
+
+  // add video object to the DOM
 
   // create the broadcaster element using the given settings 
   const parent = document.querySelector('a-scene');
-  var newBroadcaster = document.createElement('a-gltf-model');
-  newBroadcaster.setAttribute('id', streamId);
-  newBroadcaster.setAttribute('gltf-model', gltfModel);
-  newBroadcaster.setAttribute('scale', scale);
-  newBroadcaster.setAttribute('position', position);
-  newBroadcaster.setAttribute('rotation', rotation);
-  parent.appendChild(newBroadcaster);
-
-  console.log(newBroadcaster);
+  parent.appendChild(video);
   // add event listener for model loaded: 
-  newBroadcaster.addEventListener('model-loaded', () => {
-    var mesh = newBroadcaster.getObject3D('mesh');
-    mesh.traverse((node) => {
-      // search the mesh's children for the face-geo
-      if (node.isMesh && node.name == 'face-geo') {
-        // create video texture from video element
-        var texture = new THREE.VideoTexture(video);
-        texture.minFilter = THREE.LinearFilter; 
-        texture.magFilter = THREE.LinearFilter; 
-        texture.flipY = false;
-        // set node's material map to video texture
-        node.material.map = texture
-        node.material.color = new THREE.Color();
-        node.material.metalness = 0;
-      }
-    });
-  }); 
+  
 }
 
 function connectStreamToVideo(agoraStream, video) {
