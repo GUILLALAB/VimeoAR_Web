513e4795b348da0aaf2e4f2dbe6e080d241ab593
Guillaouic Franck <FOXspin@hotmail.fr>
2022-05-12 17:59:00 +0200

    first commit

--- assets/js/webvr-broadcast-client.js
+++ assets/js/webvr-broadcast-client.js
@@ -135,6 +135,7 @@ rtcClient.on('unmute-video', (evt) => {
 // join a channel
 function joinChannel() {
   const token = "006e76fbfaa876b4c68a5d92d92aa6ad3b1IAAiEEyND9UL5hWVRIUBfzhmGn/eUEblfYR9lZeKBYzwIlE4yRUAAAAAEAA5DUG6Zkx+YgEAAQBmTH5i";
+  alert(name);
   // set the role
   rtcClient.setClientRole('audience', () => {
     console.log('Client role set to audience');
