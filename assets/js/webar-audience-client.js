35a70b23aa2c6745e627f8370db7ce178286f18f
Guillaouic Franck <FOXspin@hotmail.fr>
2022-05-12 14:18:34 +0200

    first commit

--- assets/js/webar-audience-client.js
+++ assets/js/webar-audience-client.js
@@ -81,7 +81,7 @@ client.on('unmute-video', (evt) => {
 
 // join a channel
 function joinChannel() {
-  const token = "006e76fbfaa876b4c68a5d92d92aa6ad3b1IABSPSluiqAXCPY7LufgzJBqqLcaenrAW7Sm2XRn07vob1E4yRUAAAAAEACr2kwL/uB7YgEAAQD+4Hti";
+  const token = "006e76fbfaa876b4c68a5d92d92aa6ad3b1IAAiEEyND9UL5hWVRIUBfzhmGn/eUEblfYR9lZeKBYzwIlE4yRUAAAAAEAA5DUG6Zkx+YgEAAQBmTH5i";
 
   // set the role
   client.setClientRole('audience', () => {
