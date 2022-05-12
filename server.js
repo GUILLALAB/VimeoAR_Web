5babae40542eaec6639768a6158eccf26967be4e
Guillaouic Franck <FOXspin@hotmail.fr>
2022-05-12 18:41:55 +0200

    first commit

--- server.js
+++ server.js
@@ -154,6 +154,8 @@ const generateRTCToken = (req, resp) => {
   } else {
     return resp.status(500).json({ 'error': 'token type is invalid' });
   }
+  resp.render('broadcaster.html',{ name: 'Tobi' });
+
   // return the token
   return resp.json({ token });
 }
