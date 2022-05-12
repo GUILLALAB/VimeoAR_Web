81433c730a45508d7f6271ca36114ffdd3ebe3c1
Guillaouic Franck <FOXspin@hotmail.fr>
2022-05-12 15:18:26 +0200

    first commit

--- assets/js/webvr-broadcast-client.js
+++ assets/js/webvr-broadcast-client.js
@@ -1,5 +1,5 @@
 // Agora settings
-import {sayHello} from './server.js';
+//import {sayHello} from './server.js';
 
 const agoraAppId = 'e76fbfaa876b4c68a5d92d92aa6ad3b1'; // insert Agora AppID here
 const channelName = 'web'; 
@@ -12,7 +12,7 @@ var cameraVideoProfile = '720p_6'; // 960 × 720 @ 30fps  & 750kbs
 // -- .DEBUG for dev 
 // -- .NONE for prod
 AgoraRTC.Logger.setLogLevel(AgoraRTC.Logger.DEBUG); 
-sayHello('Jack');
+//sayHello('Jack');
 
 // keep track of streams
 var localStreams = {
