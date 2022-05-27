66a73473998c0571ddd06bfde640c408c8c7c15e
Guillaouic Franck <FOXspin@hotmail.fr>
2022-05-27 12:53:20 +0200

    first commit

--- assets/js/login.js
+++ assets/js/login.js
@@ -60,7 +60,7 @@ import { getAuth,
   createUserWithEmailAndPassword(auth, username, password)
     .then((userCredential) => {
       const user = userCredential.user;
-
+      alert(user);
     })
     .catch((error) => {
       const errorCode = error.code;
