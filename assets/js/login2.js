be43060fbe9c473e38612fd539b77fdd3675da8c
Guillaouic Franck <FOXspin@hotmail.fr>
2022-05-31 16:46:16 +0200

    first commit

--- assets/js/login2.js
+++ assets/js/login2.js
@@ -205,17 +205,7 @@ signInAnonymously(auth)
  SignUp.addEventListener('click', SignEmail);
 
  // Toggle for the button.
- const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
 
 
 const firebaseApp = initializeApp(getFirebaseConfig());
