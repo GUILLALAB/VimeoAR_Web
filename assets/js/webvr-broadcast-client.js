961b8e58391900d9ccb18a9c6cd1fd405620d969
Guillaouic Franck <FOXspin@hotmail.fr>
2022-06-03 18:33:34 +0200

    first commit

--- assets/js/webvr-broadcast-client.js
+++ assets/js/webvr-broadcast-client.js
@@ -2,7 +2,7 @@
 //import {sayHello} from './server.js';
 
 const agoraAppId = 'e76fbfaa876b4c68a5d92d92aa6ad3b1'; // insert Agora AppID here
-var channelName = 'web'; 
+var channelName = ''; 
 var streamCount = 0;
 var token = "";
 var rtmClient=null;
