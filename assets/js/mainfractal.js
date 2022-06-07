6a2341ad51977743c9a7cf7b0715a70fa90072c5
Guillaouic Franck <FOXspin@hotmail.fr>
2022-06-07 08:25:10 +0200

    first commit

--- assets/js/mainfractal.js
+++ assets/js/mainfractal.js
@@ -14,7 +14,7 @@
 			xlarge:   [ '1281px',  '1680px' ],
 			large:    [ '981px',   '1280px' ],
 			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
+			small:    [ '481px',   '800px'  ],
 			xsmall:   [ '361px',   '480px'  ],
 			xxsmall:  [ null,      '360px'  ]
 		});
