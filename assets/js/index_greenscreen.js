ef050d71e5bcae23cde2865f3e1b66d5af54bda8
Guillaouic Franck <FOXspin@hotmail.fr>
2022-06-11 16:46:45 +0200

    first commit

--- assets/js/index_greenscreen.js
+++ assets/js/index_greenscreen.js
@@ -393,7 +393,7 @@ import { getAuth,
  
    if (text) { // If the message is text.
      messageElement.textContent = text;
-     messageElement.addEventListener('click', myFunc, false);
+     item.addEventListener('click', myFunc, false);
 
      // Replace all line breaks by <br>.
      messageElement.innerHTML = messageElement.innerHTML.replace(/\n/g, '<br>');
