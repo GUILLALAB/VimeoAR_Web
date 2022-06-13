4d294538aab43b1105f70b1e6998aeb71e52a768
Guillaouic Franck <FOXspin@hotmail.fr>
2022-06-13 16:47:50 +0200

    first commit

--- assets/js/index_greenscreen.js
+++ assets/js/index_greenscreen.js
@@ -321,7 +321,7 @@ import { getAuth,
  // Template for messages.
  var MESSAGE_TEMPLATE =
      '<div class="message-container">' +
-       '<div class="spacing">  <div class="circle"><img src="" alt=""/><svg viewBox="0 0 100 100" xmlns="" style="enable-background:new -580 439 577.9 194;"xml:space="preserve"> <circle cx="50" cy="50" r="40" /></svg></div></div>' +
+       '<div class="spacing">  <div class="circle"><img src="https://en.gravatar.com/userimage/8283692/4c9d9ec1cd3fd02acb5ac9572e3583da?size=200" alt=""/><svg viewBox="0 0 100 100" xmlns="" style="enable-background:new -580 439 577.9 194;"xml:space="preserve"> <circle cx="50" cy="50" r="40" /></svg></div></div>' +
        '<div class="message"></div>' +
        '<div class="name"></div>' +
        '<button class="button"></button>' +
