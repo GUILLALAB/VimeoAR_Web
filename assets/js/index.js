15ef891083dce74f07267d87e0680d379571e8cc
Guillaouic Franck <FOXspin@hotmail.fr>
2022-06-18 16:41:29 +0200

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -206,22 +206,24 @@ import { getAuth,
    try {
      // 1 - We add a message with a loading icon that will get updated with the shared image.
 
-    /* const messageRef = await addDoc(
+     const messageRef = await addDoc(
       collection(getFirestore(), "Broadcast", docRefId, "objects"),
       {
         name: getUserName(),
         imageUrl: LOADING_IMAGE_URL,
+        userid: getUserUid(),
         profilePicUrl: getProfilePicUrl(),
         timestamp: serverTimestamp()
       }
-    );*/
+    );
 
-     const messageRef = await addDoc(collection(getFirestore(), 'object'), {
-       name: getUserName(),
-       imageUrl: LOADING_IMAGE_URL,
-       profilePicUrl: getProfilePicUrl(),
-       timestamp: serverTimestamp()
-     });
+   /* const messageRef = await addDoc(collection(getFirestore(), 'object'), { //ads
+      name: getUserName(),
+      imageUrl: LOADING_IMAGE_URL,
+      userid: getUserUid(),
+      profilePicUrl: getProfilePicUrl(),
+      timestamp: serverTimestamp()
+    });*/
  
      // 2 - Upload the image to Cloud Storage.
      const filePath = `${getAuth().currentUser.uid}/${messageRef.id}/${file.name}`;
@@ -291,14 +293,14 @@ import { getAuth,
    imageFormElement.reset();
  
    // Check if the file is an image.
-   if (!file.type.match('image.*')) {
+  /* if (!file.type.match('image.*')) {
      var data = {
        message: 'You can only share images',
        timeout: 2000
      };
      signInSnackbarElement.MaterialSnackbar.showSnackbar(data);
      return;
-   }
+   }*/
    // Check if the user is signed-in
    if (checkSignedInWithMessage()) {
      saveImageMessage(file);
