40c80982a5ca90cf8034b72d1793caf81409ea39
Guillaouic Franck <FOXspin@hotmail.fr>
2022-06-18 13:47:08 +0200

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -319,15 +319,14 @@ import { getAuth,
  export async function AddUser() {
   // Add a new message entry to the Firebase database.
   try {
-  const docRef = await addDoc(collection(getFirestore(), "Users"), {
+  
+  
+  await setDoc(doc(getFirestore(), "Users", getUserUid()), {
     name: getUserName(),
     text: getUserUid(),
     profilePicUrl: getProfilePicUrl(),
     timestamp: serverTimestamp()
   },{merge: true});
-  console.log("Document written with ID: ", docRef.id);
-
-  
   }
   
   catch(error) {
