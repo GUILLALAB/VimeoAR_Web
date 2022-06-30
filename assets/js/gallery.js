d692abc639cd4538cadafc44e84e34428585ca3d
Guillaouic Franck <FOXspin@hotmail.fr>
2022-06-30 19:05:07 +0200

    first commit

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -40,7 +40,7 @@ window.onload = function(){
     
     function loadImage(ind){
       loader.load(
-        'https://www.flickr.com/photos/franck-guill/50844480083/',
+        'https://www.flickr.com/photos/franck-guill/50844480083/' + ind + Date.now(),
         function ( texture ) {
           var image = new THREE.Mesh(
             new THREE.PlaneGeometry(1024, 512), 
