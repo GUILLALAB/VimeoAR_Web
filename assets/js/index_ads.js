a30638c9472cbf3b53d3e12bf6b3e3ef5f6dd228
Guillaouic Franck <FOXspin@hotmail.fr>
2022-06-30 17:25:31 +0200

    first commit

--- assets/js/index_ads.js
+++ assets/js/index_ads.js
@@ -464,6 +464,119 @@ import { getAuth,
      submitButtonElement.setAttribute('disabled', 'true');
    }
  }
+
+///Inventory
+
+var draggedItem = null;
+  
+function Inventory(options) {
+    // Setup Rows and Items
+    this.init = function (options) {
+        let slotSizeW = options.size.w / options.slots.w;
+        let slotSizeH = options.size.h / options.slots.h;
+
+        this.html = { inventory: options.selector };
+
+        // Build grid
+        let grid = document.createElement("grid");
+        grid.style.width = options.size.w + "px";
+        grid.style.height = options.size.h + "px";
+        grid.style.gridTemplateColumns = "1fr ".repeat(options.slots.w);
+        grid.style.gridTemplateRows = "1fr".repeat(options.slots.h);
+        grid.cells = [];
+        for (var i = 0; i < options.slots.w * options.slots.h; i++) {
+            var cell = document.createElement("cell");
+            cell.style.width = slotSizeW + "px";
+            cell.style.height = slotSizeH + "px";
+            grid.appendChild(cell);
+            grid.cells.push(cell);
+        }
+        this.html.grid = grid;
+        this.html.inventory.appendChild(this.html.grid);
+
+        // Add all items
+        let items = [];
+        options.items.forEach(function (item, index, array) {
+            items.push(item);
+            let itemEl = document.createElement("item");
+            const el = document.createElement('div');
+el.textContent = 'Hello world';
+el.setAttribute('title', 'my-title');
+itemEl.appendChild(el);
+
+            itemEl.setAttribute("draggable", true);
+            itemEl.style.background = item.color;
+            //itemEl.style.width = 100 * item.w + "%";
+            //itemEl.style.height = 100 * item.h + "%";
+           
+            itemEl.style.width = "calc((100% + 6px) * " + item.w + " - 4.9px)";
+            itemEl.style.height =  "calc((100% + 6px) * " + item.h + " - 4.9px)";
+
+            grid.cells[options.slots.w * item.x + item.y].appendChild(itemEl);
+        });
+        this.html.items = items;
+
+        let itemTags = document.querySelectorAll("item");
+        for (let i = 0; i < itemTags.length; i++) {
+            itemTags[i].addEventListener("dragstart", this.dragStart);
+            itemTags[i].addEventListener("dragend", this.dragEnd);
+        }
+
+        let cellTags = document.querySelectorAll("cell");
+        for (let i = 0; i < cellTags.length; i++) {
+            cellTags[i].addEventListener("dragover", this.dragOver);
+            cellTags[i].addEventListener("dragenter", this.dragEnter);
+            cellTags[i].addEventListener("dragleave", this.dragLeave);
+            cellTags[i].addEventListener("drop", this.dragDrop);
+        }
+    };
+
+    this.dragStart = function () {
+        this.classList.toggle("hold");
+        window.requestAnimationFrame(() => this.classList.toggle("invisible"));
+        draggedItem = this;
+    };
+    this.dragEnd = function () {
+        this.classList.toggle("hold");
+        this.classList.toggle("invisible");
+    };
+
+    this.dragOver = function (e) {
+        e.preventDefault();
+    };
+    this.dragEnter = function (e) {
+        e.preventDefault();
+        this.classList.toggle("hovered");
+    };
+    this.dragLeave = function (e) {
+        this.classList.remove("hovered");
+    };
+    this.dragDrop = function (e) {
+        this.classList.remove("hovered");
+        if (draggedItem != null) {
+            draggedItem.parentElement.removeChild(draggedItem);
+            this.appendChild(draggedItem);
+        }
+    };
+
+    this.init(options);
+}
+
+var inventory = new Inventory({
+    selector: document.querySelector("inventory"),
+    size: { w: 600, h: 150 },
+    slots: { w: 8, h: 2 },
+    items: [
+        { x: 0, y: 0, w: 1, h: 1, content: "1x1", color: "#ffd54f" },
+    { x: 0, y: 1, w: 1, h: 1, content: "1x1", color: "#ffd54f" },
+
+        { x: 0, y: 2, w: 1, h: 1, content: "1x1", color: "#66bb6a" },
+        { x: 0, y: 3, w: 1, h: 1, content: "1x1", color: "#e53835" }
+    ]
+});
+
+
+
  
  // Shortcuts to DOM Elements.
  var messageListElement = document.getElementById('messages');
