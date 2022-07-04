98e77c492306aa37167624ce7b0c78398fc7b279
Guillaouic Franck <FOXspin@hotmail.fr>
2022-07-04 11:40:19 +0200

    first commit

--- assets/js/index_ads.js
+++ assets/js/index_ads.js
@@ -620,17 +620,17 @@ import { getAuth,
   }
 
   export async function UpdateAdsObject(Idref,x,y,z) {
-try{
-  
-  const doc = doc(getFirestore(), "ads", Idref);
 
-// Set the "capital" field of the city 'DC'
+    try{
+  const docRef = doc(getFirestore(), "ads", Idref);
+setDoc(docRef, {
+  "position.x": x,
+  "position.y": y,
+  "position.z": z
+}, {
+  merge: true
+}).then(() => console.log("Document updated"));
 
-  await updateDoc(doc,{
-    "position.x": x,
-    "position.y": y,
-    "position.z": z
-  });
 } catch (error) {
 }
 }
