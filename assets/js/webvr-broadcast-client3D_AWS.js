1725b6f0db6302ae0ad588d851768f61eaaf44f3
Guillaouic Franck <FOXspin@hotmail.fr>
2023-01-25 19:00:21 +0100

    projecjt

--- assets/js/webvr-broadcast-client3D_AWS.js
+++ assets/js/webvr-broadcast-client3D_AWS.js
@@ -117,8 +117,7 @@ if (rtcClient != null) {
     evt.stream.stop(); // stop the stream
     const remoteId = evt.stream.getId();
     // Remove the 3D and Video elements that were created
-    console.log("stream-leave " +'faceVideo-' + remoteId);
-    document.getElementById('faceVideo-' + remoteId).id="video";
+    document.getElementById('faceVideo-' + localStreams.uid).id="video";
   //  document.getElementById(remoteId).remove();
   //  document.getElementById('faceVideo-' + remoteId).remove();
     streamCount--;  // Decrease count of Active Stream Count
