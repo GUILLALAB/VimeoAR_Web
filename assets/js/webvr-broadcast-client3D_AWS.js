c8394715332469131ce6f8ffcf2297ba466f464b
Guillaouic Franck <FOXspin@hotmail.fr>
2023-01-25 17:09:27 +0100

    projecjt

--- assets/js/webvr-broadcast-client3D_AWS.js
+++ assets/js/webvr-broadcast-client3D_AWS.js
@@ -49,7 +49,7 @@ export var Broadcaster =
 };
 
 var rtcClient = AgoraRTC.createClient({ mode: 'live', codec: 'vp8' }); // vp8 to work across mobile devices
-const rtmClient = AgoraRTM.createInstance(agoraAppId);
+var rtmClient = AgoraRTM.createInstance(agoraAppId);
 
 var start = document.getElementById('start');
 start.addEventListener('click', init);
@@ -155,7 +155,8 @@ function generateUUID_LiveStream() {
 
 function init() {
   // setup the RTM client and channel
-
+   rtcClient = AgoraRTC.createClient({ mode: 'live', codec: 'vp8' }); // vp8 to work across mobile devices
+   rtmClient = AgoraRTM.createInstance(agoraAppId);  
  // if (document.getElementById("myInput").value.length > 1) {
    // channelName = document.getElementById("myInput").value;
    channelName = generateUUID_LiveStream();
