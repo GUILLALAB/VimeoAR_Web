2bcd5f2ceadf7abdd7b77b9c68a59e63559245f4
Guillaouic Franck <FOXspin@hotmail.fr>
2023-01-30 15:34:01 +0100

    projecjt

--- assets/js/template_listdatavideoindex.js
+++ assets/js/template_listdatavideoindex.js
@@ -1,22 +1,22 @@
 var SI_SYMBOL = ["", "k", "M", "G", "T", "P", "E"];
 
-function abbreviateNumber(number){
+function abbreviateNumber(number) {
+  // what tier? (determines SI symbol)
+  var tier = Math.log10(Math.abs(number)) / 3 | 0;
 
-    // what tier? (determines SI symbol)
-    var tier = Math.log10(Math.abs(number)) / 3 | 0;
+  // if zero, we don't need a suffix
+  if (tier == 0) return number;
 
-    // if zero, we don't need a suffix
-    if(tier == 0) return number;
+  // get suffix and determine scale
+  var suffix = SI_SYMBOL[tier];
+  var scale = Math.pow(10, tier * 3);
 
-    // get suffix and determine scale
-    var suffix = SI_SYMBOL[tier];
-    var scale = Math.pow(10, tier * 3);
+  // scale the number
+  var scaled = number / scale;
 
-    // scale the number
-    var scaled = number / scale;
-
-    // format number and add suffix
-    return scaled.toFixed(1) + suffix;
+  // format number and add suffix
+  var formatted = scaled % 1 === 0 ? Math.round(scaled) : scaled.toFixed(1);
+  return formatted + " " + suffix;
 }
 
      function loadProducts(data){
